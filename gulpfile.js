1abccbcc0a20a95729b3678a83e07ed75e468703
lJOKERl <myweb.joker@gmail.com>
2021-06-29 19:48:40 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+	stylus = require('gulp-stylus')
+// autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('stylus', function () {
+	// Говорим Gulp откуда взять наши Stylus файлы
+	return (
+		gulp
+			.src(['./css/*.styl'])
+			// Заменяем текущий поток на кастомный, чтобы избежать умирания вотчера при ошибках в коде
+
+			// Подключаем плагин Stylus
+			.pipe(stylus())
+
+			.pipe(gulp.dest('./css'))
+	)
+})
+
+gulp.task('watch', function () {
+	gulp.watch('./css/*.styl', gulp.parallel('stylus'))
+})
